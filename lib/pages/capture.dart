777ac5962dba6905f9517929f5a0b756f4a81dd3
Mihir Aggarwal <mihir.aggarwal1000@gmail.com>
2023-07-15 23:19:53 +0530

    ml kit pose detection

--- lib/pages/capture.dart
+++ lib/pages/capture.dart
@@ -1,55 +1,81 @@
 import 'dart:io';
+import 'dart:typed_data';
 
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:camerawesome/camerawesome_plugin.dart';
 import 'package:better_open_file/better_open_file.dart';
+import 'package:flutter/rendering.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:google_mlkit_pose_detection/google_mlkit_pose_detection.dart';
 
-class Capture extends StatelessWidget {
+import '/utils/detector_view.dart';
+import '/utils/pose_painter.dart';
+
+class Capture extends StatefulWidget {
   static const id = "capture";
   Capture({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: CameraAwesomeBuilder.awesome(
-        saveConfig: SaveConfig.photo(pathBuilder: () => _path(CaptureMode.photo)),
-        onMediaTap: (mediaCapture) async {
-          OpenFile.open(mediaCapture.filePath);
-          final inputImage = InputImage.fromFilePath(mediaCapture.filePath);
-          final options = PoseDetectorOptions();
-          final poseDetector = PoseDetector(options: options);
-
-          final List<Pose> poses = await poseDetector.processImage(inputImage);
-          print("$options + $poseDetector + $poses");
-          for (Pose pose in poses) {
-            // to access all landmarks
-            pose.landmarks.forEach((_, landmark) {
-              final type = landmark.type;
-              final x = landmark.x;
-              final y = landmark.y;
-              print("hiiii $type $x $y");
-            });
-
-            // to access specific landmarks
-            final landmark = pose.landmarks[PoseLandmarkType.nose];
-          }
-          poseDetector.close();
-        },
-
-      ),
-    );
-  }
+  State<Capture> createState() => _CaptureState();
 }
 
-Future<String> _path(CaptureMode captureMode) async {
-  final Directory extDir = await getTemporaryDirectory();
-  final testDir =
-  await Directory('${extDir.path}/test').create(recursive: true);
-  final String fileExtension =
-  captureMode == CaptureMode.photo ? 'jpg' : 'mp4';
-  final String filePath =
-      '${testDir.path}/${DateTime.now().millisecondsSinceEpoch}.$fileExtension';
-  return filePath;
+class _CaptureState extends State<Capture> {
+  final PoseDetector _poseDetector =
+  PoseDetector(options: PoseDetectorOptions());
+  bool _canProcess = true;
+  bool _isBusy = false;
+  CustomPaint? _customPaint;
+  String? _text;
+  var _cameraLensDirection = CameraLensDirection.back;
+
+  @override
+  void dispose() async {
+    _canProcess = false;
+    _poseDetector.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DetectorView(
+      title: 'Pose Detector',
+      customPaint: _customPaint,
+      text: _text,
+      onImage: _processImage,
+      initialCameraLensDirection: _cameraLensDirection,
+      onCameraLensDirectionChanged: (value) => _cameraLensDirection = value,
+    );
+  }
+
+  Future<void> _processImage(InputImage inputImage) async {
+    if (!_canProcess) return;
+    if (_isBusy) return;
+    _isBusy = true;
+    setState(() {
+      _text = '';
+    });
+    final poses = await _poseDetector.processImage(inputImage);
+    if (inputImage.metadata?.size != null &&
+        inputImage.metadata?.rotation != null) {
+      final painter = PosePainter(
+        poses,
+        inputImage.metadata!.size,
+        inputImage.metadata!.rotation,
+        _cameraLensDirection,
+      );
+      print("ughhlol ${inputImage} ${inputImage.metadata} ${inputImage.metadata?.size} ${inputImage.metadata?.rotation} ${_cameraLensDirection}");
+      _customPaint = CustomPaint(painter: painter);
+    } else {
+      print("ughhh ${inputImage} ${inputImage.metadata} ${inputImage.metadata?.size} ${inputImage.metadata?.rotation} ${_cameraLensDirection}");
+      _text = 'Poses found: ${poses.length}\n\n';
+      // TODO: set _customPaint to draw landmarks on top of image
+      final painter = PosePainter(poses, Size(1280.0, 720.0), InputImageRotation.rotation270deg, _cameraLensDirection);
+      _customPaint = CustomPaint(painter: painter);
+    }
+    _isBusy = false;
+    if (mounted) {
+      setState(() {});
+    }
+  }
 }
